444abb90fb420a6e9de6b0509b23598c1211210d
Xenia CSERKUN <62146873+XeniaChe@users.noreply.github.com>
2020-09-03 17:55:41 +0300

    Step 2

--- part-1/part1-ex1.6-1.14/src/index.js
+++ part-1/part1-ex1.6-1.14/src/index.js
@@ -12,6 +12,16 @@ const App = () => {
 	const goodClickHandler = () => setGood(good + 1);
 	const neutralClickHandler = () => setNeutral(neutral + 1);
 	const badClickHandler = () => setBad(bad + 1);
+
+	let total = good + neutral + bad;
+	let avarage, positive;
+	if (total > 0) {
+		avarage = (good - bad) / total;
+		positive = good / total * 100;
+	} else {
+		avarage = 0;
+		positive = 0;
+	}
 	return (
 		<div>
 			<h1> Give feedback</h1>
@@ -22,6 +32,9 @@ const App = () => {
 			<p>Good: {good}</p>
 			<p>Neutral: {neutral}</p>
 			<p>Bad: {bad}</p>
+			<p>Total: {total}</p>
+			<p>Avarage: {avarage}</p>
+			<p>Positive: {positive}%</p>
 		</div>
 	);
 };
