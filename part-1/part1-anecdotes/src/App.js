6ab4324a3ed41cf21615eaa71f4151c3411e4f60
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-08 10:29:01 +0200

    Part-1 Anecdotes. Step 3

--- part-1/part1-anecdotes/src/App.js
+++ part-1/part1-anecdotes/src/App.js
@@ -3,16 +3,50 @@ import './App.css';
 
 const App = (props) => {
 	const [ selected, setSelected ] = useState(0);
+	const [ votes, setVotes ] = useState(new Array(6).fill(0));
 
 	const randomNumHandler = () => {
 		let random = Math.floor(Math.random() * 6);
 		setSelected(random);
 	};
 
+	const setVotesHandler = () => {
+		let newVotes = [ ...votes ];
+		newVotes[selected] += 1;
+		setVotes(newVotes);
+	};
+
+	let bestAnecdote = null;
+	let bestIndex = 0;
+	for (let vote of votes) {
+		if (vote > votes[bestIndex]) {
+			bestIndex = votes.indexOf(vote);
+		}
+	}
+
+	let votesSum = 0;
+	for (let vote of votes) {
+		votesSum += vote;
+	}
+
+	if (votesSum > 0) {
+		bestAnecdote = (
+			<p>
+				{' '}
+				{props.anecdotes[bestIndex]}{' '}
+				<strong>Has {votes[bestIndex]} votes</strong>
+			</p>
+		);
+	}
+
 	return (
 		<div>
-			<h2>{props.anecdotes[selected]}</h2>
+			<h2>Anecdot of the day</h2>
+			<p>{props.anecdotes[selected]}</p>
+			<button onClick={setVotesHandler}>vote</button>
 			<button onClick={randomNumHandler}>next anecdote</button>
+			<h2>Anecdot with most votes</h2>
+			{bestAnecdote}
 		</div>
 	);
 };
