5b99897f09647d5458d839a0c339eb53c7ae7357
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-08 10:59:10 +0200

    Part-1 Introduction Total num fixed

--- part-1/part1-introduction/src/components/Total.js
+++ part-1/part1-introduction/src/components/Total.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Total = (props) => {
-	let total =
-		props.parts[0].exercise +
-		props.parts[1].exercise +
-		props.parts[2].exercise;
+	let total = 0;
+	for (let part of props.parts) {
+		total += part.exercise;
+	}
 	return <p>The exercises total number is: {total}</p>;
 };
 export default Total;
