c1310d6f80326b1e7f0c3230c3ad70047fdd91fd
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-11 18:12:33 +0200

    part-2 Last step Separete module

--- Part-2/course-info/src/components/Course.js
+++ Part-2/course-info/src/components/Course.js
@@ -1,51 +1,47 @@
 import React from 'react';
 
-
 const Header = ({ course }) => {
-    return (
-      <h1>{course.name}</h1>
-    )
-}
+	return <h1>{course.name}</h1>;
+};
 
 const Part = (props) => {
-    return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>    
-    )
+	return (
+		<p>
+			{props.part.name} {props.part.exercises}
+		</p>
+	);
 };
 
-const Content = ({course}) => {
-  const keyCreate = (num) => {
-    return Math.random() * Math.floor(num)
-  };
+const Content = ({ course }) => {
+	const keyCreate = (num) => {
+		return Math.random() * Math.floor(num);
+	};
 
-  let parts = course.parts.map(el => <Part part={el} key={keyCreate(course.parts.length)}/>);
+	let parts = course.parts.map((el) => (
+		<Part part={el} key={keyCreate(course.parts.length)} />
+	));
 
-  return (
-    <div>
-      {parts}
-    </div>
-  );
+	return <div>{parts}</div>;
 };
 
-const Total = ({course}) => {
-    const total = course.parts.reduce((acc, value) => acc+value.exercises, 0);
-  
-    return(
-      <p><strong>Number of {total} exercises</strong></p>
-    ) 
+const Total = ({ course }) => {
+	const total = course.parts.reduce((acc, value) => acc + value.exercises, 0);
+
+	return (
+		<p>
+			<strong>Number of {total} exercises</strong>
+		</p>
+	);
 };
 
-
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header course={course}/>
-            <Content course={course}/>
-            <Total course={course}/>
-        </div>
-    );
+const Course = ({ course }) => {
+	return (
+		<div>
+			<Header course={course} />
+			<Content course={course} />
+			<Total course={course} />
+		</div>
+	);
 };
 
 export default Course;
