7992bd806f8cc907ee39348629ed6e348a5b9f03
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-11 17:48:02 +0200

    part-2 Course info Step 9

--- Part-2/course-info/src/components/Course.js
+++ Part-2/course-info/src/components/Course.js
@@ -1,7 +1,42 @@
 import React from 'react';
-import Content from './Content';
-import Header from './Header';
-import Total from './Total';
+
+
+const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+};
+
+const Content = ({course}) => {
+  const keyCreate = (num) => {
+    return Math.random() * Math.floor(num)
+  };
+
+  let parts = course.parts.map(el => <Part part={el} key={keyCreate(course.parts.length)}/>);
+
+  return (
+    <div>
+      {parts}
+    </div>
+  );
+};
+
+const Total = ({course}) => {
+    const total = course.parts.reduce((acc, value) => acc+value.exercises, 0);
+  
+    return(
+      <p><strong>Number of {total} exercises</strong></p>
+    ) 
+};
+
 
 const Course = ({course}) => {
     return (
