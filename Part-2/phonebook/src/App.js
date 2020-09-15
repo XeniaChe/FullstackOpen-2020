97dc0f78e83097f035c1f69fcb78e40123ecd432
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-15 12:34:29 +0200

    Part-2 Phonebook Step 3

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -4,15 +4,24 @@ import './App.css';
 const App = () => {
 	const [ persons, setPersons ] = useState([ { name: 'Arto Hellas' } ]);
 	const [ newName, setNewName ] = useState('');
+	const [ newNumber, setNewNumber ] = useState('');
 
-	const setNewnameHandler = (event) => {
+	const setNewNameHandler = (event) => {
 		let newName = event.target.value;
 		setNewName(newName);
 	};
 
-	const addNewnameHandler = (event) => {
+	const setNewNumberHandler = (event) => {
+		const newNumber = event.target.value;
+		setNewNumber(newNumber);
+	};
+
+	const addNewContactHandler = (event) => {
 		event.preventDefault();
-		const personsCopy = persons.concat({ name: newName });
+		const personsCopy = persons.concat({
+			name: newName,
+			number: newNumber
+		});
 
 		//newName repeating check
 		let namesArr = persons.map((el) => el.name);
@@ -29,9 +38,12 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form onSubmit={addNewnameHandler}>
+			<form onSubmit={addNewContactHandler}>
 				<div>
-					name: <input onChange={setNewnameHandler} />
+					name: <input onChange={setNewNameHandler} />
+				</div>
+				<div>
+					number: <input onChange={setNewNumberHandler} />
 				</div>
 				<div>
 					<button type='submit'>add</button>
