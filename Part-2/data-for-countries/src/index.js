7c1c43bd9b2c8ddc4e2ba98f86bb4adc094b7e01
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-09-16 22:05:02 +0200

    Part-2 Data-for-countries Step 2

--- Part-2/data-for-countries/src/index.js
+++ Part-2/data-for-countries/src/index.js
@@ -9,6 +9,8 @@ const App = () => {
 
 	const [ searchQuery, setSearchQuery ] = useState('');
 
+	const [ showView, setShowView ] = useState({ show: false });
+
 	useEffect(() => {
 		const loadData = async () => {
 			try {
@@ -23,29 +25,87 @@ const App = () => {
 		loadData();
 	}, []);
 
+	//reset showView condition on query change to search index of pressed el-t in refreshed  filtered countries list
+	useEffect(
+		() => {
+			return setShowView({ show: false });
+		},
+		[ searchQuery ]
+	);
+
 	const setSearchQueryHandler = (event) => {
 		let inputData = event.target.value;
 		setSearchQuery(inputData);
 	};
 
+	//Filter countries after query set
 	let filteredCountries = countries.filter((el) =>
 		el.name.toLowerCase().includes(searchQuery)
 	);
 
-	const countriesToShow =
-		searchQuery.length > 0 ? filteredCountries : countries;
-
-	const countryList =
-		searchQuery.length > 0 && filteredCountries.length > 10 ? (
-			<p> too many matchs, specify another filter</p>
-		) : (
-			<ul>
-				{countriesToShow.map((el) => <li key={el.name}>{el.name}</li>)}
-			</ul>
+	//Button to Show Country Preview Handler
+	const showCountryDetailHandler = (code) => {
+		let index = filteredCountries.findIndex(
+			(el) => el.numericCode === code
 		);
+		setShowView({ show: true, code: code, index: index });
+	};
 
-	const contrySingle =
-		searchQuery.length > 0 ? (
+	//Country Preview
+	let countryView = null;
+	if (showView.show && showView.index < filteredCountries.length) {
+		countryView = (
+			<div>
+				<h2>{filteredCountries[showView.index].name} </h2>
+				<p>capital: {filteredCountries[showView.index].capital}</p>
+				<p>
+					population: {filteredCountries[showView.index].population}
+				</p>
+				<h2>Languages:</h2>
+
+				<ul>
+					{filteredCountries[showView.index].languages.map((el) => (
+						<li key={el.iso639_1}>{el.name}</li>
+					))}
+				</ul>
+				<img
+					src={filteredCountries[showView.index].flag}
+					length='150px'
+					height='150px'
+					alt='country flag'
+				/>
+			</div>
+		);
+	}
+
+	//Countries List Render
+	let countryList =
+		filteredCountries.length > 0 ? (
+			<ul>
+				{filteredCountries.map((el) => (
+					<li key={el.numericCode}>
+						{el.name} {' '}
+						{searchQuery.length > 0 ? (
+							<button
+								onClick={() =>
+									showCountryDetailHandler(el.numericCode)}
+							>
+								show
+							</button>
+						) : null}
+						{showView.code === el.numericCode ? countryView : null}
+					</li>
+				))}
+			</ul>
+		) : null;
+
+	if (searchQuery.length > 0 && filteredCountries.length > 10) {
+		countryList = <p> too many matches, specify another filter</p>;
+	}
+
+	//Single Country Render
+	let contrySingle =
+		filteredCountries.length > 0 && searchQuery.length > 0 ? (
 			<div>
 				<h2>{filteredCountries[0].name} </h2>
 				<p>capital: {filteredCountries[0].capital}</p>
